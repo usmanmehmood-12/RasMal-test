88c1b83465b93fab84365f8e4c27801d6c3ee801
usmanmehmood-12 <usmanmehmood12@gmail.com>
2022-08-22 17:00:46 +0500

    Add: The project code files
    the project code files have been added along with readme.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('')
+  const [listValue, setListValue] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('value: ', value)
+    setListValue([...listValue, {
+      id: listValue.length,
+      value: value
+    }])
+    console.log('listValue: ', listValue)
+  }
+  const deleteItem = (index) => () =>
+    setListValue((listValues) => listValues.filter((_, i) => i !== index));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ display: 'flex', justifyContent: "center" }}>
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => setValue(e.target.value)} />
+          <button>Submit</button>
+        </form>
+      </div>
+      <div style={{ display: 'flex', justifyContent: "center" }}>
+        <ul>
+          {listValue.map((listValue) =>
+            <li key={listValue.id} onClick={deleteItem(listValue.id)}>{listValue.value} </li>
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
